ed66660434f067cf0aee699dac13ae0afab67ac3
Richard Graham <grahamr.912@googlemail.com>
2018-03-27 10:01:38 +0100

    serve public folder

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var favicon = require('serve-favicon');
 var path = require('path');
 
 Object.assign=require('object-assign')
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, "public")));
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
-
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
